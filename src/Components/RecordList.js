b65df3eca4bfd0bc85871ec941a9e4c6e9304609
raylin009 <raylin009@gmail.com>
2021-08-08 22:19:13 -0700

    wepack now compil to dist

--- src/Components/RecordList.js
+++ src/Components/RecordList.js
@@ -13,18 +13,20 @@ const RecordBRC = ({type}) => {
     addEntries([...entries, entry])
   }
   return (
-    <div className="recordlist">
-      <h3>{type}:</h3>
-      {
-        entries.length ?
-          entries
-            .sort((a,b)=>(b.time-a.time))
-            .map((ele,i)=>{ 
-              return <h5 key={Date.now()+i}>{ele.name}</h5>
-            })
-          : <h5>who's gonna be the first</h5>
-      }
-      <UsrList addEntry={handleEntries}/>
+    <div className="recordlist-container">
+      <h3 className="recordList-title">{type}:</h3>
+      <div className="recordlist">
+        {
+          entries.length ?
+            entries
+              .sort((a,b)=>(a.time-b.time))
+              .map((ele,i)=>{ 
+                return <h5 key={Date.now()+i}>{ele.name}</h5>
+              })
+            : <h5>who's gonna be the first</h5>
+        }
+      </div>
+      <UsrList className='usr-list' addEntry={handleEntries}/>
     </div>
   )
 };
