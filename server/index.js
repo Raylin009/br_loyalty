2d47c2c1cbc7cf0091ab5254cb61ed98879773ec
raylin009 <raylin009@gmail.com>
2021-08-10 11:50:54 -0700

    schema can load

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, "..", "dist")));
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// app.get('', function (req, res) {
+//   res.send('Hello World')
+// })
+
+
  
 app.listen(3000)
