54c132d6b9ee070f32575d3a7a821ad7177bec5c
raylin009 <raylin009@gmail.com>
2021-08-13 16:46:54 -0700

    can get employee name

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-
+const {getEmployeeNameP} = require('../database/util');
 app.use(express.static(path.join(__dirname, "..", "dist")));
- 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 // app.get('', function (req, res) {
 //   res.send('Hello World')
 // })
 
+app.get('/employee_name', async(req, res) => {
+  //require employee_id req.query.id
+  //return name or nothing
+  // getEmployeeName(req.query.id, res.send)
+  const name = await getEmployeeNameP(3277923)
+  res.send(name)
+  
+  // res.send("tested")
+})
 
  
 app.listen(3000)
